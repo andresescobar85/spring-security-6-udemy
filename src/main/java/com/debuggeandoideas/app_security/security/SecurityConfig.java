916c85e196ca2db33c33ffa8ea5d160e369f16b3
andresescobar85 <andresfelipe.escobar@hotmail.com>
2023-11-01 01:03:51 -0500

    03/password_encoders

--- src/main/java/com/debuggeandoideas/app_security/security/SecurityConfig.java
+++ src/main/java/com/debuggeandoideas/app_security/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -57,7 +58,7 @@ public class SecurityConfig {
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-		
+		return new BCryptPasswordEncoder();
 	}
+	
 }
