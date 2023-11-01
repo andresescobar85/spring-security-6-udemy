aa7c68859723adf0d4b61ec6ec41e860e86d42d8
andresescobar85 <38162454+andresescobar85@users.noreply.github.com>
2023-10-31 23:40:01 -0500

    Revert "02/custom_users-inMemory_and_jdbc"

--- db/sql/data.sql
+++
@@ -1,4 +0,0 @@
------------------data------------------
-insert into customers (email, pwd, rol) VALUES
-  ('super_user@debuggeandoieas.com', 'to_be_encoded', 'admin'),
-  ('basic_user@debuggeandoieas.com', 'to_be_encoded', 'user');
