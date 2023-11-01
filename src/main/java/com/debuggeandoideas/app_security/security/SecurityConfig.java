abfe520c3410a1dbb8672f7a0a66ef565da355fe
andresescobar85 <andresfelipe.escobar@hotmail.com>
2023-11-01 16:28:34 -0500

    05/cors-csrf

--- src/main/java/com/debuggeandoideas/app_security/security/SecurityConfig.java
+++ src/main/java/com/debuggeandoideas/app_security/security/SecurityConfig.java
@@ -1,21 +1,22 @@
 package com.debuggeandoideas.app_security.security;
 
-import javax.sql.DataSource;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -25,16 +26,21 @@ public class SecurityConfig {
 	/****CONFIGURACION POR DEFECTO DEL SPRING SECURITY************/
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+		
+		var requestHandler = new CsrfTokenRequestAttributeHandler();
+		requestHandler.setCsrfRequestAttributeName("_csrf");
 		http.authorizeHttpRequests(auth -> 
 		     auth.requestMatchers("/loans","/balance","/accounts","/cards")
 				.authenticated()
 				.anyRequest().permitAll())
 				.formLogin(Customizer.withDefaults())
 				.httpBasic(Customizer.withDefaults());
-		http.cors(AbstractHttpConfigurer::disable);
-		http.csrf(AbstractHttpConfigurer::disable);
-		
+		http.cors(cors -> corsConfigurationSource());
+		http.csrf(csrf -> csrf
+				.csrfTokenRequestHandler(requestHandler)
+				.ignoringRequestMatchers("/welcome","/about_us")
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+		.addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);	
 		return http.build();
 
 	}
@@ -63,6 +69,24 @@ public class SecurityConfig {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		
+		var config = new CorsConfiguration();
+		//config.setAllowedOrigins(List.of("http://localhost:4200/","http://my-app.com"));
+		config.setAllowedOrigins(List.of("*"));
+		//config.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+		config.setAllowedMethods(List.of("*"));
+		config.setAllowedHeaders(List.of("*"));
+		
+		var source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**",config);
+		
+		return source;
+		
 	}
 	
 }
