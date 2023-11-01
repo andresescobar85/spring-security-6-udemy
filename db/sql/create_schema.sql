40ef197b744c2e1a710aea069172778b5b29e250
andresescobar85 <andresfelipe.escobar@hotmail.com>
2023-10-31 23:28:35 -0500

    02/custom_users-inMemory_and_jdbc

---
+++ db/sql/create_schema.sql
@@ -0,0 +1,6 @@
+----------------schema-----------------
+create table customers(
+                          id bigserial primary key,
+                          email varchar(50) not null,
+                          pwd varchar(500) not null,
+                          rol varchar(20) not null);
